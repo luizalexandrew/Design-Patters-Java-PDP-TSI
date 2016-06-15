173ef552b4a59954c0286c5aa09e6b503f67bfb9
luizalexandrew <luiz.alexandre@live.com>
2016-06-15 20:31:53 -0300

    Composite

---
+++ src/br/com/luizalexandrew/pdp/composite/ArquivoVideo.java
@@ -0,0 +1,8 @@
+package br.com.luizalexandrew.pdp.composite;
+
+public class ArquivoVideo extends ArquivoComponent {
+	 
+    public ArquivoVideo(String nomeDoArquivo) {
+        this.nomeDoArquivo = nomeDoArquivo;
+    }
+}
